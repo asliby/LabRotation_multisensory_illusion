d967befca7e2349a9ecdcc52819c3fa74e5498a1
Alexa <asliby@uos.de>
2016-10-10 22:04:14 +0200

    basils additions and our shit

--- illusion.py
+++ illusion.py
@@ -3,8 +3,12 @@ import numpy as np
 import random
 import os
 import sys
+import pygame
+
+from psychopy import prefs
+prefs.general['audioLib'] = ['pygame']
 from psychopy.visual import GratingStim, RatingScale, TextStim, ShapeStim
-from psychopy import visual, core, event, data
+from psychopy import visual, core, event, data, sound
 #from numpy.random import random, randint, shuffle, sample
 from numpy import sin, cos, tan, log, log10, pi, average, sqrt, std, deg2rad, rad2deg, linspace, asarray
 
@@ -22,8 +26,98 @@ fixation = visual.TextStim(win=win, ori=0, name='fixation',
     color='white', colorSpace='rgb', opacity=1,
     depth=0.0)
 
-for frameN in range(10,100):
-    if 30 <= frameN < 110:
-        fixation.draw()
-        circle.draw()
+
+#pygame.mixer.pre_init(44100,-16,2,2048)
+#pygame.init()
+
+beep = p.sound.Sound(value = 3500.0, secs = 0.007, octave = 3)
+
+"""
+for frameN in range(57):
+    fixation.draw()
+    beep.play()
+    win.flip()
+"""
+
+
+FPS = 60
+
+def callsound():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(7):
+        if frameN == 0:
+            beep.play()
+        fpsClock.tick(FPS)
+
+
+def flash():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(17):
+        fixation.draw(win)
+        if frameN % 30  ==  0:
+            circle.draw(win)
         win.flip()
+        fpsClock.tick(FPS)
+
+def soa():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(16):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
+
+
+
+
+option = [1]
+
+
+optionNum = random.choice(option)
+print optionNum
+
+if optionNum == 1:
+    callsound()
+    soa()
+    flash()
+    
+
+
+"""for i in range(10):
+    optionNum = random.choice(option)
+    print optionNum
+
+    if optionNum == 1:
+        for frameN in range(23):
+            fixation.draw(win)
+            beep.play()
+            win.flip()
+
+        for frameN in range(17):
+            fixation.draw(win)
+            if frameN % 30  ==  0:
+                circle.draw(win)
+            win.flip()
+
+    elif optionNum == 2:
+        #optionNum ==1
+        for frameN in range(50):
+            fixation.draw(win)
+            win.flip()
+
+        for frameN in range(17):
+            fixation.draw(win)
+            if frameN % 30 == 0:
+                circle.draw(win)
+            win.flip()
+
+    elif optionNum ==3:
+        #optionNum ==2
+        for frameN in range(50):
+            fixation.draw(win)
+            win.flip()
+
+        for frameN in range(17):
+            fixation.draw(win)
+            if frameN % 30 == 0:
+                circle.draw(win)
+            win.flip()"""
