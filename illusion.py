0d1cfb10c9ab7a1183ebb5762fc0114c6d1725b9
Alexa <asliby@uos.de>
2016-10-11 19:52:41 +0200

    updated from Tuesday

--- illusion.py
+++ illusion.py
@@ -30,16 +30,25 @@ fixation = visual.TextStim(win=win, ori=0, name='fixation',
 #pygame.mixer.pre_init(44100,-16,2,2048)
 #pygame.init()
 
+#beep parameters
 beep = p.sound.Sound(value = 3500.0, secs = 0.007, octave = 3)
+#instruction for beeps
+instruction_B = p.visual.TextStim(win = win, ori = 0, text = 'Number of Beeps',
+font = 'Arial Narrow', height = 0.1, alignVert = 'top', alignHoriz = 'left', color = (1, 1, 1))
+#instruction for flashes
+instruction_F = p.visual.TextStim(win = win, ori = 0, text = 'Number of Flashes',
+font = 'Arial Narrow', height = 0.1, alignVert = 'bottom', color = (1, 1, 1))
 
-"""
-for frameN in range(57):
-    fixation.draw()
-    beep.play()
-    win.flip()
-"""
+instruction_IB = p.visual.TextStim(win = win, ori = 0, text = 'Beep Block',
+font = 'Arial Narrow', height = 0.1, alignVert = 'top', pos = (0,0), color = (1, 1, 1))
 
+instruction_IF = p.visual.TextStim(win = win, ori = 0, text = 'Flash Block',
+font = 'Arial Narrow', height = 0.1, alignVert = 'top', pos = (0,0), color = (1, 1, 1))
 
+instruction_J = p.visual.TextStim(win = win, ori = 0, text = 'Joint Block',
+font = 'Arial Narrow', height = 0.1, alignVert = 'top', pos = (0,0), color = (1, 1, 1))
+
+#keys for beeps
 FPS = 60
 
 def callsound():
@@ -59,65 +68,176 @@ def flash():
         win.flip()
         fpsClock.tick(FPS)
 
-def soa():
+def soa_B0():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(57):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
+
+def soa_F0():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(50):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
+
+def soa_F1():
     fpsClock = pygame.time.Clock()
     for frameN in range(16):
         fixation.draw(win)
         win.flip()
         fpsClock.tick(FPS)
 
+def soa_B2():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(24):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
 
+def soa_F2():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(19):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
 
+def soa_B3():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(21):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
 
-option = [1]
+def soa_F3():
+    fpsClock = pygame.time.Clock()
+    for frameN in range(22):
+        fixation.draw(win)
+        win.flip()
+        fpsClock.tick(FPS)
 
+#possible combination array
 
-optionNum = random.choice(option)
-print optionNum
+conditions = [1, 2, 3]
+for i in range(1):
+    conditionsNum = random.choice(conditions)
+    if conditionsNum == 1:
+        instruction_IB.draw()
+        win.flip()
+        keys_G = p.event.waitKeys(keyList = ["return"])
+    if conditionsNum == 2:
+        instruction_IF.draw()
+        win.flip()
+        keys_G = p.event.waitKeys(keyList = ["return"])
+    if conditionsNum == 3:
+        instruction_J.draw()
+        win.flip()
+        keys_G = p.event.waitKeys(keyList = ["return"])
 
-if optionNum == 1:
-    callsound()
-    soa()
-    flash()
-    
+option = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-
-"""for i in range(10):
+for i in range(2):
+    #choose random combination
+    core.wait(1)
     optionNum = random.choice(option)
     print optionNum
 
+    #1B 1F
     if optionNum == 1:
-        for frameN in range(23):
-            fixation.draw(win)
-            beep.play()
-            win.flip()
-
-        for frameN in range(17):
-            fixation.draw(win)
-            if frameN % 30  ==  0:
-                circle.draw(win)
-            win.flip()
-
+        callsound()
+        soa_F1()
+        flash()
+    #1B 2F
     elif optionNum == 2:
-        #optionNum ==1
-        for frameN in range(50):
-            fixation.draw(win)
-            win.flip()
-
-        for frameN in range(17):
-            fixation.draw(win)
-            if frameN % 30 == 0:
-                circle.draw(win)
-            win.flip()
-
-    elif optionNum ==3:
-        #optionNum ==2
-        for frameN in range(50):
-            fixation.draw(win)
-            win.flip()
-
-        for frameN in range(17):
-            fixation.draw(win)
-            if frameN % 30 == 0:
-                circle.draw(win)
-            win.flip()"""
+        callsound()
+        soa_F1()
+        flash()
+        soa_F0()
+        flash()
+    #1B 3F
+    elif optionNum == 3:
+        callsound()
+        soa_F1()
+        flash()
+        soa_F0()
+        flash()
+        soa_F0()
+        flash()
+    #2B 1F
+    elif optionNum == 4:
+        callsound()
+        soa_F1
+        flash()
+        soa_B2()
+        callsound()
+    #2B 2F
+    elif optionNum == 5:
+        callsound()
+        soa_F1
+        flash()
+        soa_B2()
+        callsound()
+        soa_F2()
+        flash()
+    #2B 3F
+    elif optionNum == 6:
+        callsound()
+        soa_F1()
+        flash()
+        soa_B2()
+        callsound()
+        soa_F2
+        flash()
+        soa_F0()
+        flash()
+    #3B 1F
+    elif optionNum == 7:
+        callsound()
+        soa_F1()
+        flash()
+        soa_B2()
+        callsound()
+        soa_B0()
+        callsound()
+    #3B 2F
+    elif optionNum == 8:
+        callsound()
+        soa_F1()
+        flash()
+        soa_B2
+        callsound()
+        soa_F2()
+        flash()
+        soa_B3()
+        callsound()
+    #3B 3F
+    elif optionNum == 9:
+        callsound()
+        soa_F1()
+        flash()
+        soa_B2()
+        callsound()
+        soa_F2()
+        flash()
+        soa_B3()
+        callsound()
+        soa_F3
+        flash()
+    win.flip()
+    #thisresp = None
+    while True:
+        instruction_B.draw()
+        instruction_F.draw()
+        win.flip()
+        keys_B = p.event.waitKeys(keyList = ["q", "w", "e"])
+        keys_F = p.event.waitKeys(keyList = ["i", "o", "p"])
+        if keys_B == "q" or keys_B == "w" or keys_B == "e":
+            break
+        if keys_F == "i" or keys_F == "o" or keys_F == "p":
+            break
+        print keys_B
+        print keys_F
+        win.flip()
+        break
+    core.wait(2)
