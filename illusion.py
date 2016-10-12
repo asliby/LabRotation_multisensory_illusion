5b223ee23dd7585cdc6e41ce1821a8252c9342de
baumgartkg <kbaumgart@uni-osnabrueck.de>
2016-10-12 12:30:00 +0200

    fixed trials

--- illusion.py
+++ illusion.py
@@ -34,19 +34,19 @@ fixation = visual.TextStim(win=win, ori=0, name='fixation',
 beep = p.sound.Sound(value = 3500.0, secs = 0.007, octave = 3)
 #instruction for beeps
 instruction_B = p.visual.TextStim(win = win, ori = 0, text = 'Number of Beeps',
-font = 'Arial Narrow', height = 0.1, alignVert = 'top', alignHoriz = 'left', color = (1, 1, 1))
+font = 'Arial Narrow', height = 0.1, color = (1, 1, 1))
 #instruction for flashes
 instruction_F = p.visual.TextStim(win = win, ori = 0, text = 'Number of Flashes',
-font = 'Arial Narrow', height = 0.1, alignVert = 'bottom', color = (1, 1, 1))
+font = 'Arial Narrow', height = 0.1, color = (1, 1, 1))
 
 instruction_IB = p.visual.TextStim(win = win, ori = 0, text = 'Beep Block',
-font = 'Arial Narrow', height = 0.1, alignVert = 'top', pos = (0,0), color = (1, 1, 1))
+font = 'Arial Narrow', height = 0.1, pos = (0,0), color = (1, 1, 1))
 
 instruction_IF = p.visual.TextStim(win = win, ori = 0, text = 'Flash Block',
-font = 'Arial Narrow', height = 0.1, alignVert = 'top', pos = (0,0), color = (1, 1, 1))
+font = 'Arial Narrow', height = 0.1, pos = (0,0), color = (1, 1, 1))
 
 instruction_J = p.visual.TextStim(win = win, ori = 0, text = 'Joint Block',
-font = 'Arial Narrow', height = 0.1, alignVert = 'top', pos = (0,0), color = (1, 1, 1))
+font = 'Arial Narrow', height = 0.1, pos = (0,0), color = (1, 1, 1))
 
 #keys for beeps
 FPS = 60
@@ -119,144 +119,151 @@ def soa_F3():
 
 #possible combination array
 
+
 conditions = [1, 2, 3]
-for i in range(1):
-    conditionsNum = random.choice(conditions)
-    if conditionsNum == 1:
-        instruction_IB.draw()
-        win.flip()
-        keys_G = p.event.waitKeys(keyList = ["return"])
-    elif conditionsNum == 2:
-        instruction_IF.draw()
-        win.flip()
-        keys_G = p.event.waitKeys(keyList = ["return"])
-    elif conditionsNum == 3:
-        instruction_J.draw()
-        win.flip()
-        keys_G = p.event.waitKeys(keyList = ["return"])
-
-option = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for i in range(2):
-    #choose random combination
-    core.wait(1)
-    optionNum = random.choice(option)
-    print optionNum
-
-    #1B 1F
-    if optionNum == 1:
-        callsound()
-        soa_F1()
-        flash()
-    #1B 2F
-    elif optionNum == 2:
-        callsound()
-        soa_F1()
-        flash()
-        soa_F0()
-        flash()
-    #1B 3F
-    elif optionNum == 3:
-        callsound()
-        soa_F1()
-        flash()
-        soa_F0()
-        flash()
-        soa_F0()
-        flash()
-    #2B 1F
-    elif optionNum == 4:
-        callsound()
-        soa_F1
-        flash()
-        soa_B2()
-        callsound()
-    #2B 2F
-    elif optionNum == 5:
-        callsound()
-        soa_F1
-        flash()
-        soa_B2()
-        callsound()
-        soa_F2()
-        flash()
-    #2B 3F
-    elif optionNum == 6:
-        callsound()
-        soa_F1()
-        flash()
-        soa_B2()
-        callsound()
-        soa_F2
-        flash()
-        soa_F0()
-        flash()
-    #3B 1F
-    elif optionNum == 7:
-        callsound()
-        soa_F1()
-        flash()
-        soa_B2()
-        callsound()
-        soa_B0()
-        callsound()
-    #3B 2F
-    elif optionNum == 8:
-        callsound()
-        soa_F1()
-        flash()
-        soa_B2
-        callsound()
-        soa_F2()
-        flash()
-        soa_B3()
-        callsound()
-    #3B 3F
-    elif optionNum == 9:
-        callsound()
-        soa_F1()
-        flash()
-        soa_B2()
-        callsound()
-        soa_F2()
-        flash()
-        soa_B3()
-        callsound()
-        soa_F3
-        flash()
-    win.flip()
-    #thisresp = None
-    while True:
+for i in range(3):
+    random.shuffle(conditions)
+    print conditions
+    for i in range(2): #number of trials
+        conditionsNum = conditions[0]
+        print conditionsNum
         if conditionsNum == 1:
-            instruction_B.draw()
+            instruction_IB.draw()
             win.flip()
-            keys_B = p.event.waitKeys(keyList = ["q", "w", "e"])
-            if keys_B == "q" or keys_B == "w" or keys_B == "e":
-                break
-            print keys_B
-            win.flip()
-            break
+            keys_G = p.event.waitKeys(keyList = ["return"])
         elif conditionsNum == 2:
-            instruction_F.draw()
+            instruction_IF.draw()
             win.flip()
-            keys_F = p.event.waitKeys(keyList = ["i", "o", "p"])
-            if keys_F == "i" or keys_F == "o" or keys_F == "p":
-                break
-            win.flip()
-            break
+            keys_G = p.event.waitKeys(keyList = ["return"])
         elif conditionsNum == 3:
-            instruction_B.draw()
+            instruction_J.draw()
             win.flip()
-            keys_B = p.event.waitKeys(keyList = ["q", "w", "e"])
-            if keys_B == "q" or keys_B == "w" or keys_B == "e":
-                break
-            instruction_F.draw()
+            keys_G = p.event.waitKeys(keyList = ["return"])
+
+        option = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        for i in range(1):
+            #choose random combination
+            core.wait(1)
+            optionNum = random.choice(option)
+            print optionNum
+
+            #1B 1F
+            if optionNum == 1:
+                callsound()
+                soa_F1()
+                flash()
+            #1B 2F
+            elif optionNum == 2:
+                callsound()
+                soa_F1()
+                flash()
+                soa_F0()
+                flash()
+            #1B 3F
+            elif optionNum == 3:
+                callsound()
+                soa_F1()
+                flash()
+                soa_F0()
+                flash()
+                soa_F0()
+                flash()
+            #2B 1F
+            elif optionNum == 4:
+                callsound()
+                soa_F1
+                flash()
+                soa_B2()
+                callsound()
+            #2B 2F
+            elif optionNum == 5:
+                callsound()
+                soa_F1
+                flash()
+                soa_B2()
+                callsound()
+                soa_F2()
+                flash()
+            #2B 3F
+            elif optionNum == 6:
+                callsound()
+                soa_F1()
+                flash()
+                soa_B2()
+                callsound()
+                soa_F2
+                flash()
+                soa_F0()
+                flash()
+            #3B 1F
+            elif optionNum == 7:
+                callsound()
+                soa_F1()
+                flash()
+                soa_B2()
+                callsound()
+                soa_B0()
+                callsound()
+            #3B 2F
+            elif optionNum == 8:
+                callsound()
+                soa_F1()
+                flash()
+                soa_B2
+                callsound()
+                soa_F2()
+                flash()
+                soa_B3()
+                callsound()
+            #3B 3F
+            elif optionNum == 9:
+                callsound()
+                soa_F1()
+                flash()
+                soa_B2()
+                callsound()
+                soa_F2()
+                flash()
+                soa_B3()
+                callsound()
+                soa_F3
+                flash()
             win.flip()
-            keys_F = p.event.waitKeys(keyList = ["i", "o", "p"])
-            if keys_F == "i" or keys_F == "o" or keys_F == "p":
-                break
-            print keys_B
-            print keys_F
-            win.flip()
-            break
-    core.wait(2)
+            #thisresp = None
+            while True:
+                if conditionsNum == 1:
+                    instruction_B.draw()
+                    win.flip()
+                    keys_B = p.event.waitKeys(keyList = ["q", "w", "e"])
+                    if keys_B == "q" or keys_B == "w" or keys_B == "e":
+                        break
+                    print keys_B
+                    win.flip()
+                    break
+                elif conditionsNum == 2:
+                    instruction_F.draw()
+                    win.flip()
+                    keys_F = p.event.waitKeys(keyList = ["i", "o", "p"])
+                    if keys_F == "i" or keys_F == "o" or keys_F == "p":
+                        break
+                    win.flip()
+                    break
+                elif conditionsNum == 3:
+                    instruction_B.draw()
+                    win.flip()
+                    keys_B = p.event.waitKeys(keyList = ["q", "w", "e"])
+                    if keys_B == "q" or keys_B == "w" or keys_B == "e":
+                        break
+                    instruction_F.draw()
+                    win.flip()
+                    keys_F = p.event.waitKeys(keyList = ["i", "o", "p"])
+                    if keys_F == "i" or keys_F == "o" or keys_F == "p":
+                        break
+                    print keys_B
+                    print keys_F
+                    win.flip()
+                    break
+            core.wait(2)
+
+    del conditions[0]
